5d2b10fa9415ef9bdeced11306e4c91797f239ed
Анастасия <shishlakovanastya@gmail.com>
2020-11-04 22:25:42 +0300

    fixed style menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
-$(document).ready(() => {
-    console.log(`document ready`)
-})
+// $(document).ready(() => {
+//     console.log(`document ready`)
+// })
+$('.header__menu-icon').click(function () {
+    $('.header__menu-icon').toggleClass('menu-active');
+});
